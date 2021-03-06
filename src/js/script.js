8745e3d07c87d22635f7af7fa728839c2430face
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-03-06 11:42:36 +0100

    Add class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -378,6 +378,27 @@
 
     }
   }
+
+  class Cart {
+    constructor (element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+
+      console.log ('new cart', thisCart);
+    }
+
+    getElements (element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
+
   const app = {
 
     initMenu: function () {
