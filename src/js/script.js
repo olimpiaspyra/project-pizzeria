30580ab958a4254535e4f11ec747db8d8d7b1215
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-03-06 12:02:54 +0100

    Add initCart

--- src/js/script.js
+++ src/js/script.js
@@ -416,6 +416,13 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function () {
+      const thisApp = this;
+
+      const cartElem = document.querySelector (select.containerOf.cart);
+      thisApp.cart = new Cart (cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       // console.log('*** App starting ***');
@@ -426,6 +433,8 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
+
     },
   };
 
