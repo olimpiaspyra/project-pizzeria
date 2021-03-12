7554d9d2761363e57a8ffe320a4ef8571693d392
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-03-12 23:40:59 +0100

    Add AJAX and API

--- src/js/script.js
+++ src/js/script.js
@@ -71,6 +71,11 @@
     cart: {
       defaultDeliveryFee: 20,
     },
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
 
   const templates = {
@@ -714,14 +719,36 @@
       // console.log ('thisAppData:', thisApp.data);
 
       for (let productData in thisApp.data.products) {
-        new Product (productData, thisApp.data.products[productData]);
+        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
     },
 
     initData: function () {
       const thisApp = this;
 
-      thisApp.data = dataSource;
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch (url)
+        .then (function (rawResponse) {
+          return rawResponse.json();
+        })
+        .then (function (parsedResponse) {
+          console.log ('parsedResponse', parsedResponse);
+
+          /* save parsedResponse as thisApp.data.products */
+
+          thisApp.data.products = parsedResponse;
+
+          /* execute initMenu method */
+
+          thisApp.initMenu();
+
+        });
+
+      console.log ('thisApp.data', JSON.stringify (thisApp.data));
+
     },
 
     initCart: function () {
@@ -740,7 +767,6 @@
       // console.log('templates:', templates);
 
       thisApp.initData();
-      thisApp.initMenu();
       thisApp.initCart();
 
     },
