ff466eee23f400e84a4167ce57352b0adb127917
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-03-21 21:20:50 +0100

    Add API to booking (bug)

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,5 @@
-import {templates, select} from '../settings.js';
+import {templates, select, settings} from '../settings.js';
+import utils from '../utils.js';
 import AmountWidget from './AmountWidget.js';
 import DatePicker from './DatePicker.js';
 import HourPicker from './HourPicker.js';
@@ -10,7 +11,53 @@ class Booking {
 
     thisBooking.render (element);
     thisBooking.initWidgets ();
+    thisBooking.getData ();
 
+  }
+
+  getData () {
+    const thisBooking = this;
+
+    const startDateParam =  settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
+    const params = {
+
+      booking: [
+        startDateParam,
+        endDateParam,
+      ],
+
+      eventsCurrent: [
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
+      ],
+
+      eventsRepeat: [
+        settings.db.repeatParam,
+        endDateParam,
+      ],
+    };
+
+    // console.log ('getData params', params);
+
+    const urls = {
+      booking:        settings.db.url + '/' + settings.db.booking + '/' + params.booking.join('&'),
+      eventsCurrent:  settings.db.url + '/' + settings.db.event   + '/' + params.eventsCurrent.join('&'),
+      eventsRepeat:   settings.db.url + '/' + settings.db.event   + '/' + params.eventsRepeat.join('&'),
+    };
+
+    console.log ('getData urls', urls);
+
+    fetch(urls.booking)
+      .then(function (bookingsResponse) {
+        return bookingsResponse.json();
+      })
+      .then(function(bookings) {
+        console.log (bookings);
+      });
   }
 
   render (element) {
