a63d5bced340f7dfe5cfae71773546b0b8f7d645
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-03-23 21:17:37 +0100

    Fix API to booking

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -44,19 +44,33 @@ class Booking {
     // console.log ('getData params', params);
 
     const urls = {
-      booking:        settings.db.url + '/' + settings.db.booking + '/' + params.booking.join('&'),
-      eventsCurrent:  settings.db.url + '/' + settings.db.event   + '/' + params.eventsCurrent.join('&'),
-      eventsRepeat:   settings.db.url + '/' + settings.db.event   + '/' + params.eventsRepeat.join('&'),
+      booking:        settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
+      eventsCurrent:  settings.db.url + '/' + settings.db.event   + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:   settings.db.url + '/' + settings.db.event   + '?' + params.eventsRepeat.join('&'),
     };
 
-    console.log ('getData urls', urls);
+    // console.log ('getData urls', urls);
 
-    fetch(urls.booking)
-      .then(function (bookingsResponse) {
-        return bookingsResponse.json();
+    Promise.all ([
+      fetch(urls.booking),
+      fetch(urls.eventsCurrent),
+      fetch(urls.eventsRepeat),
+    ])
+      .then(function (allResponses) {
+        const bookingsResponse = allResponses[0];
+        const eventsCurrentResponse = allResponses[1];
+        const eventsRepeatResponse = allResponses[2];
+
+        return Promise.all ([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
       })
-      .then(function(bookings) {
+      .then(function([bookings, eventsCurrent, eventsRepeat]) {
         console.log (bookings);
+        console.log (eventsCurrent);
+        console.log (eventsRepeat);
       });
   }
 
